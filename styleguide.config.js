3dc73e3519d3edc716779569e1068bd545b717ff
Benjamin Dehli <superelg@gmail.com>
2020-09-11 15:35:50 +0200

    Add config for styleguide

---
+++ styleguide.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+module.exports = {
+	title: "Frontend Components",
+	styleguideDir: "docs",
+	contextDependencies: [path.resolve(__dirname, 'src/lib/components')],
+	require: [
+		path.join(__dirname, 'src/styleguide/styleguide.js')
+	],
+	sections: [
+		{name: 'Introduction', content: './src/styleguide/introduction.md'},
+	    {name: 'Components', components: './src/lib/components/**/*.js'}
+	],
+	template: {
+		head: {
+			links: [
+				{
+					rel: 'stylesheet',
+					href: 'src/styleguide/styleguide.css'
+				}
+			]
+		},
+		favicon: 'src/styleguide/favicon.ico'
+	},
+	theme: {
+		fontFamily: {
+			base: '"Open Sans", sans-serif'
+		}
+	}
+};
